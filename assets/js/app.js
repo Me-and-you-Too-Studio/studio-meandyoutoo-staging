3f4ea7e80217fabb7a22904f8d3caadc5c9aa4cc
ITS <contact@intotheshift.io>
2026-07-09 16:01:53 +0200

    code par claude

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const path = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('[data-nav]').forEach(link => {
-    const href = link.getAttribute('href');
-    if (href === path || (path === 'index.html' && href === 'index.html')) link.setAttribute('aria-current','page');
-  });
-})();
